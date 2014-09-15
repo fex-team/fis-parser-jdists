6ff481913aa82b89de5fa34c96b80f1bad44d6eb
Zswang <wjhu111@21cn.com>
2014-09-15 20:08:54 +0800

    完善用例

---
+++ test/fixtures/function-text.js
@@ -0,0 +1,11 @@
+void function() {
+  $('#panel').innerHTML = format(function() {/*!
+<div>
+  <a href="#{url}">#{title}</a>
+  <button>cancel</button><button>download</button>
+</div>
+  */}, {
+    title: 'loading',
+    url: 'http://www.baidu.com/'
+  });
+}();

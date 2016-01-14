bf3841089ea6b0075c1c3a8e494ad0bd0166cb9e
zswang <wjhu111@21cn.com>
2016-01-15 00:20:20 +0800

    update jdists@1.3.3

--- src/jdists.js
+++ src/jdists.js
@@ -32,7 +32,7 @@ module.exports = function (content, file, conf) {
     fromString: true,
     path: file.origin,
     remove: conf.remove,
-    trigger: conf.trigger || (global.fis && fis.project.currentMedia()),
+    trigger: conf.trigger || (global.fis && global.fis.project.currentMedia()),
     config: conf.config
   });
 
